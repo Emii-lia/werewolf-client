8a40daf83ada66ee0396ea130a441fe03c1c5f80
Emii-lia <fiaromiangaly@gmail.com>
2026-02-27 21:51:55 +0300

    refactor(home): optimizing imports

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,4 @@
-import Image from "next/image";
-import styles from "./page.module.css";
-import Link from "next/dist/client/link";
+import Link from "next/link";
 import { Button } from "@/components/ui/button";
 import { Card } from "@/components/ui/card";
 
