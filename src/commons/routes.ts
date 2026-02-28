0d0d26be3786a0947dc0d1c3eea169ee94078945
Emii-lia <fiaromiangaly@gmail.com>
2026-02-28 10:58:17 +0300

    feat(proxy): implementing proxy and protected routes handler

---
+++ src/commons/routes.ts
@@ -0,0 +1,14 @@
+const protectedRoutes = [
+  "/menu",
+  "/room",
+  "/roles",
+]
+
+const publicRoutes = [
+  "/",
+  "/login",
+  "/signup",
+  "/how-to"
+]
+
+export { protectedRoutes, publicRoutes };
---
+++ src/proxy.ts
@@ -0,0 +1,19 @@
+import { NextRequest, NextResponse } from "next/server";
+import { protectedRoutes, publicRoutes } from "@/commons/routes";
+
+export default function proxy(req: NextRequest) {
+  const { cookies, nextUrl } = req;
+  const authToken = cookies.get('auth-token')
+
+  const isProtected = protectedRoutes.some(route => nextUrl.pathname.startsWith(route));
+
+  if (isProtected && !authToken) {
+    return NextResponse.redirect(new URL('/login', req.url));
+  }
+
+  return NextResponse.next();
+}
+
+export const config = {
+  matcher: '/((?!api|trpc|_next|_vercel).*)'
+};
