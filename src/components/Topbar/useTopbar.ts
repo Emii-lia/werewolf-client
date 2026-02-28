fd635b87334b600e24b224ef5cae2bd41a6e18da
Emii-lia <fiaromiangaly@gmail.com>
2026-02-28 11:56:40 +0300

    feat(auth): clearing session when token is expired

--- src/components/Topbar/useTopbar.ts
+++ src/components/Topbar/useTopbar.ts
@@ -1,14 +1,31 @@
 import { useAuthStore } from "@/stores/auth";
-import { useRouter } from "next/navigation";
+import { usePathname, useRouter } from "next/navigation";
+import { useEffect } from "react";
+import { jwtDecode } from "jwt-decode";
+import { protectedRoutes } from "@/commons/routes";
 
 const useTopbar = () => {
   const router = useRouter();
-  const { user, isGuest, logout } = useAuthStore();
+  const pathname = usePathname()
+  const { user, isGuest, logout, token } = useAuthStore();
 
   const handleLogout = () => {
     logout();
     router.push("/login");
   };
+
+  useEffect(() => {
+    if (token) {
+      let tokenObj = jwtDecode<{ sub: string, exp: number }>(token)
+      const currentTime = Math.floor(Date.now() / 1000);
+      if (tokenObj.exp < currentTime) {
+        logout();
+        if (protectedRoutes.some(p => pathname.startsWith(p))) {
+          router.push("/login");
+        }
+      }
+    }
+  }, [token]);
 
   return {
     user,
